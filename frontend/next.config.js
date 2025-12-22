b76a88361f0850429dfe45f329d43747c66df2ec
m97j <mmnkjiae@gmail.com>
2025-12-23 00:55:33 +0900

    config(next): allow i.imgur.com in image domains

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["storage.googleapis.com", "blob.core.windows.net"],
+    domains: ["storage.googleapis.com", "blob.core.windows.net", "i.imgur.com"],
   },
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
