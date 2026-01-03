24a43fdac15bb968e53288cfca5aadef583eb195
m97j <mmnkjiae@gmail.com>
2026-01-04 02:20:51 +0900

    update header

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
   return (
     <header className="navbar bg-base-100 shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto flex w-full items-center justify-center relative">
-        {/* 왼쪽 로고 */}
+        {/* Left: Logo */}
         <div className="absolute left-4">
           <Link href="/" className="btn btn-ghost normal-case text-xl">
             <Image
@@ -22,29 +22,29 @@ export default function Header() {
           </Link>
         </div>
 
-        {/* 가운데 메뉴 */}
+        {/* Middle : Menus */}
         <ul className="menu menu-horizontal text-sm font-medium gap-x-6">
           <li>
             <Link href="/">Home</Link>
           </li>
           <li>
-            <Link href="/blogs">Blogs</Link>
-          </li>
-          <li>
             <Link href="/projects">Projects</Link>
           </li>
           <li>
-            <Link href="/notes">Study Notes</Link>
+            <Link href="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link href="/notes">Notes</Link>
           </li>
           <li>
             <Link href="/info">Information</Link>
           </li>
         </ul>
 
-        {/* 오른쪽 다크모드 토글 */}
-        <div className="absolute right-4">
+        {/* Right: Darkmode Toggle [not used now] */}
+        {/* <div className="absolute right-4">
           <DarkModeToggle />
-        </div>
+        </div> */}
       </div>
     </header>
   );
